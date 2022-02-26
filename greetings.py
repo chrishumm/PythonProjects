34cc0a0a376fa0b6b237070ee6ccd412b704c1a7
Chris <chumm@riseup.net>
2022-02-26 19:03:28 +0800

    Update!

--- greetings.py
+++ greetings.py
@@ -1,32 +1,31 @@
 GreetingMessage= ["Hello\nWhat is your name?", "It is nice to meet you, ", "","What is your age?"]
+name = 2
 
 def greet():
- counter= 0;
+ counter = 0
  while counter < 4:
   if counter < 1:
    print(GreetingMessage[counter])
   elif counter == 1:
-   GreetingMessage[counter+1] = input()
-   print(GreetingMessage[counter] + GreetingMessage[counter+1])
-   counter+=1
+   GreetingMessage[name] = input()
+   print(GreetingMessage[1] + GreetingMessage[name])
   elif counter < 3:
    print(GreetingMessage[counter]) 
+   
   counter+=1
 
 def askAge():
  print(GreetingMessage[3])
  myAge = input()
- print('You will be ' + str(int(myAge)+ 1) + ' in a year.')
+ print('You will be ' + str(int(myAge)+1) + ' in a year.')
  
 def calcNameLength():
- myName = GreetingMessage[2]
- print('The length of your name is: ')
- print(len(myName))
+ print('The length of your name is: '+ str(len(GreetingMessage[name])))
 
- 
  
 greet()
 calcNameLength()
 askAge()
+exit()
 
 
