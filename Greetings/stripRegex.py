b1de4726cde81d761490fbc11e04d4318f0c8cc9
Chris Humm <chumm@riseup.net>
2022-05-23 13:19:52 +0800

    improved regex strip

--- Greetings/stripRegex.py
+++ Greetings/stripRegex.py
@@ -2,9 +2,17 @@
 # stripRegex.py - a regex version of the strip() function
 import re, pyperclip
 
+def striptheword(word, word_to_strip):
+    if(word_to_strip == None):
+        word_to_strip = "\s"
+    stripRegex = re.compile(rf'{word_to_strip}*|{word_to_strip}*$')
+    output = re.sub(stripRegex, "",word)
+
+    return output
+
 word = pyperclip.paste()
-stripRegex = re.compile(rf'\s*|\s*$')
-output = re.sub(stripRegex, "",word)
-print(output)
+print("Input regex, number or letter to strip")
+strip_this = input()
+print(striptheword(word, strip_this))
 
 #removes all spaces with re.sub
