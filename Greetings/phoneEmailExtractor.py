fc133a106feb5884db9687468bbd50e7466e3f4a
Chris Humm <chumm@riseup.net>
2022-05-23 08:02:59 +0800

    added phone number verification

--- Greetings/phoneEmailExtractor.py
+++ Greetings/phoneEmailExtractor.py
@@ -5,16 +5,31 @@ emailRe = re.compile(r'[a-z0-9]+@\w+\.[a-z]{2,3}', re.IGNORECASE)
 numbers = re.compile(r'[\+]?[44]?\d{8,10}', re.VERBOSE)
 list_of_emails = emailRe.findall(copied_text)
 list_of_phone_numbers = numbers.findall(copied_text)
-print('List of Emails:')
+matches = []
+matches.append('List of Emails: ')
 for count in range(len(list_of_emails)):
-    print(list_of_emails[count] + '\n')
+    matches.append(list_of_emails[count])
 
-print('List of phone numbers')
+matches.append('List of Phone Numbers: ')
 
 for count in range(len(list_of_phone_numbers)):
-    print(list_of_phone_numbers[count] + '\n')
+    phoneNum = list_of_phone_numbers[count]
+    if phoneNum[:3] == '+44':
+        phoneNum = ('0' + phoneNum[3:])
+        matches.append(phoneNum)
+    else:
+        matches.append(list_of_phone_numbers[count])
+
+
+
 '''8 Eastern Way
 Milford on Sea
 LYMINGTON
 SO41 0TB
 '''
+
+
+if len(matches) > 0:
+    pyperclip.copy('\n'.join(matches))
+    print('Copied!')
+    print('\n'.join(matches))
